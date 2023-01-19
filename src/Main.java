34ccb9560dfc04de0c617474fabb0ac7cf56e82f
user <user@gmail.com>
2023-01-19 04:47:12 +0200

    Add myNumberProcessing

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,8 @@ public class Main {
         int b = (int)(40 + Math.random()*10);
         System.out.println("a - " + a + "\n" + "b - " + b);
     }
+    public static void myNumberProcessing(int a, int b){
+        System.out.println("Sum a & b - " + (a + b));
+        System.out.println("Product a & b - " + (a * b));
+    }
 }
