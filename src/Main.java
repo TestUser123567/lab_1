ec1f408cb1012c8859c668308f9c2e0313643e41
user <user@gmail.com>
2023-01-19 04:46:06 +0200

    Create project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int a = (int)(20 + Math.random()*10);
+        int b = (int)(40 + Math.random()*10);
+        System.out.println("a - " + a + "\n" + "b - " + b);
+    }
+}
